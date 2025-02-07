f6fff28a621482142b8e017153d49e13baa792a6
KuangHcmUT <quang.nguyenqnvnbk@hcmut.edu.vn>
2025-02-07 21:18:06 +0700

    Remove React strict mode and experimental app directory settings from Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
-    reactStrictMode: true,
-    experimental: {
-        appDir: true, // If using the App Router
-    },
+
 };
 
 export default nextConfig;
