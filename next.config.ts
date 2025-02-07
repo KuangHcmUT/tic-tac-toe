5d29b5c035e5cf7d8d9ee33d7d42bcb24dbe0881
KuangHcmUT <quang.nguyenqnvnbk@hcmut.edu.vn>
2025-02-07 21:16:13 +0700

    Enable React strict mode and experimental app directory in Next.js configuration; remove obsolete Vercel configuration file.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    reactStrictMode: true,
+    experimental: {
+        appDir: true, // If using the App Router
+    },
 };
 
 export default nextConfig;
